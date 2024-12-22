c7d2c07a38c9214566174b2feb0386041fd44314
BugHunter-X <askzenai@gmail.com>
2024-12-22 07:38:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MyComponent() {
+  const [count, setCount] = React.useState(0);
+
+  const incrementCount = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  return (
+    <View>
+      <Text>Count: {count}</Text>
+      <Button title="Increment" onPress={incrementCount} />
+    </View>
+  );
+}
+
+export default MyComponent;
